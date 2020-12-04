ca1f68e627b804d39c514a9ef0bdf57c23a5a90c
FullStackJunkie <quadruplestak@gmail.com>
2020-12-04 18:34:17 -0500

    Phase One

---
+++ connect-four.js
@@ -0,0 +1,10 @@
+import { Game } from "./game.js"
+
+let game;
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("player-1-name").addEventListener("keyup", ()=> {
+        if ()
+        document.getElementById("new-game").setAttribute("disabled", false)
+
+    })
+})
---
+++ game.js
@@ -0,0 +1,13 @@
+
+class Game {
+    constructor(name1, name2) {
+        this.name1 = playerOneName
+        this.name2 = playerTwoName
+    }
+
+    getGame () {
+        return `Player 1 ${this.name1} vs. Player 2 ${this.name2}`
+    }
+}
+
+module.exports = Game
