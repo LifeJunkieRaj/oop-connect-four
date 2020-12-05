9c0152b0d17c9db7acb34d6473975f281e795f3b
Bryan Burns <bryanburns93@gmail.com>
2020-12-05 04:18:45 -0500

    Add updateUI

--- connect-four.js
+++ connect-four.js
@@ -1,13 +1,28 @@
 import { Game } from "./game.js"
 let game;
+function updateUI(){
+    if (game) {
+        document.getElementById('board-holder').classList.remove('is-invisible')
+        document.getElementById('game-name').innerHTML = game.getName()
+    }
+}
 window.addEventListener("DOMContentLoaded", () => {
     let p1Name =  document.getElementById('player-1-name')
     let p2Name = document.getElementById('player-2-name')
+    let btnNewGame = document.getElementById('new-game')
     function enableDisableNewGame() {
-        document.getElementById('new-game').disabled = p1Name.value === '' || p2Name.value === '' ? true : false
+        btnNewGame.disabled = p1Name.value === '' || p2Name.value === '' ? true : false
     }
-   p1Name.addEventListener('keyup', enableDisableNewGame)
-   p2Name.addEventListener('keyup', enableDisableNewGame)
+    p1Name.addEventListener('keyup', enableDisableNewGame)
+    p2Name.addEventListener('keyup', enableDisableNewGame)
 
-
+    btnNewGame.addEventListener('click', () => {
+        game = new Game(p1Name.value, p2Name.value)
+        btnNewGame.disabled = true
+        updateUI()
+    })
+    document.getElementById('#click-targets').addEventListener('click', e => {
+        game.playInColumn()
+        updateUI()
+    })
 })
