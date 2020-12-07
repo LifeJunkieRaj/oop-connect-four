f708d958a33aaaeac123a6fc43f6a1d97ca816f6
Bryan Burns <bryanburns93@gmail.com>
2020-12-06 22:41:29 -0500

    add hover effect

--- connect-four.js
+++ connect-four.js
@@ -1,9 +1,11 @@
-import { Game } from "./game.js"
+import { Game } from './game.js'
 let game;
 function updateUI(){
     if (game) {
         document.getElementById('board-holder').classList.remove('is-invisible')
         document.getElementById('game-name').innerHTML = game.getName()
+        document.getElementById('click-targets').classList.add(game.curPlayer === 1 ? 'black' : 'red')
+        document.getElementById('click-targets').classList.remove(game.curPlayer === 1 ? 'red' : 'black')
     }
 }
 window.addEventListener("DOMContentLoaded", () => {
@@ -19,10 +21,11 @@ window.addEventListener("DOMContentLoaded", () => {
     btnNewGame.addEventListener('click', (e) => {
         game = new Game(p1Name.value, p2Name.value)
         btnNewGame.disabled = true
-        updateUI(e.target)
+        updateUI()
     })
     document.getElementById('click-targets').addEventListener('click', e => {
-        game.playInColumn()
+        game.playInColumn(e.target.id.split('-'))
+        console.log(e)
         updateUI()
     })
 })
